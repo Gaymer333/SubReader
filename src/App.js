7cf9a154ddd538ab851abcad1a421902c16245d1
Gaymer333 <elflover333@gmail.com>
2020-11-15 21:12:08 +0100

    Spelingcheck

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ function App() {
       <TypePicker/>
       <h2>Made by Peter Andresen</h2>
       <i>Coding test solution for SubReader</i>
-      <p>A good test. Make you show skills in understanding of variable types, react state, react render and recursive functions.</p>
-      <p>I made my solution in TypeScript. This makes some of the code a little harder to read, but gave me a eaiser time finding type errors beforehand.</p>
-      <p>I also use "useState" instead of "extends React.Component". This is for me eaiser to read, and feels more moden</p>
-      <p>If i did have more time and other to spare with, would i considder the following:</p>
+      <p>A good test. Makes you show skills in understanding of variable types, react state, react render and recursive functions.</p>
+      <p>I made my solution in TypeScript. This makes some of the code a little harder to read, but gave me an easier time finding type errors beforehand.</p>
+      <p>I also use "useState" instead of "extends React.Component". This is, for me, easier to read, and feels more modern</p>
+      <p>If I had more time and others to spar with, I would consider the following:</p>
       <ul>
-        <li>Should i make the "prods" definsing diffrents? It can be hard to read.</li>
-        <li>I remake the element, everytime i need to add/update/delete a child. Could i do this in a better way?</li>
-        <li>I used "useState" and did not use "class [CLASSNAME] extends React.Component". Is this the right call? Do this make the recursive datahandling worse?</li>
-        <li>The "TypePicker" code is seperated over 7 functions. Should i try to collect some of the code under less functions?</li>
+        <li>Should I make the "prods" definition different? It can be hard to read.</li>
+        <li>Should I remake the element, everytime i need to add/update/delete a child? Could I do this in a better way?</li>
+        <li>I used "useState" and did not use "class [CLASSNAME] extends React.Component". Is this the right call? Does this make the recursive datahandling worse?</li>
+        <li>The "TypePicker" code is separated over 7 functions. Should I try to collect some of the code in fewer functions?</li>
         <li>OR</li>
-        <li>Should i seperate the function into more files?</li>
-        <li>I made a Emnu with the Types. But would a static list with type code and type label have been better?</li>
-        <li>Should i have made diffrent children variables for Object and Array, instead of using the same list?</li>
-        <li>I could even make a "ArrayGetter" that picked the first element in the Object list. But would that be bad UX?</li>
-        <li>Will this data logic work optimal for saving to and from JSON?</li>
+        <li>Should I separate the function into more files?</li>
+        <li>I made an Enum with the Types. But would a static list with type code and type label have been better?</li>
+        <li>Should I have made different children variables for Object and Array, instead of using the same list?</li>
+        <li>I could even make an "ArrayGetter" that picked the first element in the Object list. But would that be bad UX?</li>
+        <li>Will this data logic work optimally for saving to and from JSON?</li>
       </ul>
-      <p>If you look in the code, have i comment "CUO" several places. Here is there code, that if i had the time, i would test for optimasation</p>
+      <p>If you look in the code, have I commented "CUO" several places. Here there is code, that if I had the time, I would test for optimization</p>
     </div>
   );
 }
--- src/components/TypePicker/TypePicker.tsx
+++ src/components/TypePicker/TypePicker.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react'
 
-// Enum over the diffrent types.
+// Enum over the different types.
 enum Types {
     s = 's',
     n = 'n',
@@ -10,7 +10,7 @@ enum Types {
     o = 'o',
 }
 
-// A string, is maybe not of the "Types", so in that cause, we need to return an defualt.
+// A string, is maybe not of the "Types", so in that case, we need to return a default.
 const GetTypeOrDefault = (string: string): Types => Object.values(Types).find(t => t === string) || Types.s
 
 // Interface to help keep order in the recursive data
@@ -30,13 +30,13 @@ const NewDefaultTypePickerElement = () => {
     }}
 }
 
-// Starting point for the code. This is the only funtion that is exported.
+// Starting point for the code. This is the only function that is exported.
 const TypePickerWrapper = () => {
 
     const [product, setProduct] = useState<TypePickerElement>(NewDefaultTypePickerElement())
 
     // We could move this down into the "<TypePicker ... />" directly.
-    // But i feel this is eaiser to read.
+    // But I feel this is easier to read.
     const renderString = (result: TypePickerElement) => setProduct(result);
 
     return <div className="TypePickerMainWrapper" >
@@ -50,7 +50,7 @@ const TypePickerWrapper = () => {
     </div>
 }
 
-// When we need to reander, this function will be the render root element.
+// When we need to render, this function will be the render root element.
 const TypePickerRender = (props: {
     product: TypePickerElement
 }) => {
@@ -62,14 +62,14 @@ const TypePickerRender = (props: {
 // This could have been made even shorter:
 // ...}) => <pre>{"type " + TypePickerElementToString(props.product)}</pre>
 
-// When we need to convert it to code syntax, we need to hange every element.
+// When we need to convert it to code syntax, we need to change every element.
 const TypePickerElementToString = (element: TypePickerElement, lastChild: boolean = true, childLevel: number = 0): string => {
     let r: string = "";
-    // With this, we can reuse the same function for both the root and under diclrastions
+    // With this, we can reuse the same function for both the root and under declarations
     const setterChar = childLevel === 0 ? " =" : ":"
 
     // An feature that was not in your code.
-    // I feeled that this made it eaiser to see where we have forgotten name of the variable.
+    // I felt that this made it easier to see where we have forgotten the name of the variable.
     r += element.name ? element.name : "[NAME MISSING]"
 
     r += setterChar + " " + TypePickerElementTypeToString(element, childLevel);
@@ -79,31 +79,31 @@ const TypePickerElementToString = (element: TypePickerElement, lastChild: boolea
     return r
 }
 
-// The type to stirng converter logic is seperated, cause Array needs the informations without name.
+// The type to stirng converter logic is separated, cause Array needs the informations without name.
 const TypePickerElementTypeToString = (element: TypePickerElement, childLevel: number): string => {
     let r: string = "";
 
     switch (element.type) {
-        // Not logic neede for string
+        // No logic needed for string
         case Types.s:
             r += "string"
         break;
         
-        // Not logic neede for number
+        // No logic needed for number
         case Types.n:
             r += "number"
         break;
         
-        // Array is unik, cause it can never have more the one child.
+        // Array is unique, cause it can never have more than one child.
         case Types.a:
             const arrayElement = element.children[0];
-            // It should not happen. But can we end up with a element without childen? In that case we need to fall back on a defualt
+            // It should not happen. But can we end up with a element without childen? In that case we need to fall back on a default
             // "CUO"
             const typeAsString: string = arrayElement ? TypePickerElementTypeToString(arrayElement, childLevel + 1) : Types.s;
             r += "Array<" + typeAsString + ">"
         break;
         
-        // Object need always to work as an many child element. Even if it have one or none.
+        // Object needs to always work as a many child element. Even if it has one or none.
         case Types.o:
             // Set strings that need to be used more time.
             // Prefix will grow the longer down in the data tree we get.
@@ -133,52 +133,52 @@ const TypePicker = (props: {
     // Here we use useState to make sure that the rerender works. But we work with recursive functions, so...
     const [localElement, setLocalElement] = useState<TypePickerElement>(props.element)
 
-    // ... we need to make sure that the parent always are up-to-date, and...
+    // ... we need to make sure that the parent is always up-to-date, and...
     useEffect(() => {
         props.updateParent(localElement);
     }, [localElement])
 
     // ... since the parent can delete it's children, the order that children come in can change.
-    // But a god and bad part of React, is that is will just reuse the <option /> tags.
-    // So we need to update the child manuraly, if a previres sibline have been removed.
+    // But a good and bad part of React, is that is will just reuse the <option /> tags.
+    // So we need to update the child manually, if a previous sibling has been removed.
     useEffect(() => {
         if (props.element.name !== localElement.name || props.element.type !== localElement.type) {
             setLocalElement(props.element)
         }
     }, [props.element])
 
-    // We need handler to handle the changes. Name is string to string. No big deal.
+    // We need handler to handle the changes. Name is string to string. 
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setLocalElement({ ...localElement, name: e.target.value })
     }
 
-    // From here, I start to make clone everytime i need to make changes to the "localElement."
-    // This is maybe more heavy on the clint then needed.
-    // But it insures that we do not have any State data problems.
+    // From here, I start to clone everytime I need to make changes to the "localElement."
+    // This is maybe more heavy on the client than needed.
+    // But it ensures that we do not have any State data problems.
 
-    // But when the selecter change, do we need to look at the array.
+    // But when the selector changes, we need to look at the array.
     const handleSelectChange = (e: React.ChangeEvent<{ value: unknown }>) => {
         const newElement = { ...localElement, type: GetTypeOrDefault(e.target.value as string) }
 
-        // If we have an array, we need to eacher:
+        // If we have an array, we need to either:
         if (newElement.type === Types.a) {
             if (!newElement.children.length) {
-                // Add an child, if there is non
+                // Add a child, if there is none
                 newElement.children.push(NewDefaultTypePickerElement())
             } else if(newElement.children.length > 1) {
-                // Or remove down to 1, if there is more then 1.
+                // Or remove down to 1, if there is more than 1.
                 newElement.children.splice(1)
                 newElement.children[0].name = ""
             }
         }
-        // Object is eaiser. Just need an child, if it have none.
+        // Object is easier. Just need a child, if it has none.
         else if (newElement.type === Types.o && !newElement.children.length) newElement.children.push(NewDefaultTypePickerElement())
 
         setLocalElement(newElement)
     }
 
-    // Sometimes do our child change on there own.
-    // In that case, do we need to send that information up in the recursive tree,
+    // Sometimes our child changes on its own.
+    // In that case, we need to send that information up in the recursive tree,
     // so the root function can update the render.
     const handleChildChange = (element: TypePickerElement, index: number) => {
         const newElement = { ...localElement }
@@ -186,7 +186,7 @@ const TypePicker = (props: {
         setLocalElement(newElement)
     }
 
-    // Add a child. With the default element code, is this easy.
+    // Add a child. With the default element code, this is easy.
     const handleAddChild = () => {
         const newElement = { ...localElement }
         newElement.children.push(NewDefaultTypePickerElement())
@@ -214,7 +214,7 @@ const TypePicker = (props: {
                     <option value={Types.o}>Object</option>
                 </select>
             </div>
-            {/* This is a little messy. Could maybe be streamedlined. But it works. */}
+            {/* This is a little messy. Could maybe be streamlined. But it works. */}
             {/* "CUO" */}
             { (localElement.type === Types.o || localElement.type === Types.a) &&
                 <div className="TypePickerChildren">
